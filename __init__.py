fd122b984e5ee70ddadc2fb7092ff4be7707a4e6
咕咕型小r <418648118@qq.com>
2022-07-19 14:08:51 +0800

    优化持仓图片显示宽度

--- __init__.py
+++ __init__.py
@@ -170,7 +170,7 @@ async def _(event: MessageEvent, bot: Bot, state: T_State, arg: Message = Comman
             await sell_stock.send(MessageSegment.image(await text_to_pic("你还什么都没买呢！", width=300)))
         txt = convert_stocks_to_md_table(my_stocks)
         logger.info(txt)
-        await sell_stock.finish(MessageSegment.image(await md_to_pic(f"{txt}", width=1200)))
+        await sell_stock.finish(MessageSegment.image(await md_to_pic(f"{txt}", width=1000)))
 
     # windows下使用这套逻辑(风控差一点)
     if platform.system().lower() == 'windows':
